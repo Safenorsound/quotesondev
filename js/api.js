a81b950982c58dae7033a96bfd7c164aac0882ea
Safenorsound <ijroback@gmail.com>
2018-09-15 19:32:59 -0700

    update values to post method sections

--- js/api.js
+++ js/api.js
@@ -5,14 +5,14 @@
    * Ajax-based random post fetching & History API
    */
 
-  //  Code provided in class threw slack:
-
   // Code adapted from slide 34: http://red-wdp.herokuapp.com/slides/ajax-in-wordpress-wp-rest-api-slides/#34
 
   // Ajax call changed from 'post' to 'get'
 
   $('#new-quote-button').on('click', function(event) {
     event.preventDefault();
+
+    // create a variable to store the last page. In the done method for the ajax request. Use the history api pushstate. MDN history.pushState
 
     $.ajax({
       method: 'get',
@@ -36,40 +36,45 @@
 
       // Creat an "if" statement if there is no person or website. Can use quote.source.length: true or false.
 
-      // To modify the history slug in the URL: https://developer.mozilla.org/en-US/docs/Web/API/History_API
-      history.pushState(null, null, api_vars.home_url + '/' + quote.slug);
-
       // Popstate: when the active history changes.
       // This is so the client can go back to a previous state.
-      window.addEventListener('popstate', function(e) {
+      window.addEventListener('popstate', function(event) {
         window.location.replace(url);
         url = home_url;
+
+        // To modify the history slug in the URL: https://developer.mozilla.org/en-US/docs/Web/API/History_API
+        history.pushState(null, null, api_vars.home_url + '/' + quote.slug);
       });
     });
   });
 
-  $.ajax({
-    method: 'post',
-    url: api_vars.rest_url + 'wp/v2/posts/',
-    data: {
-      status: 'pending',
-      title: $('#quote-author').val(),
-      content: $('#quote-content').val(),
-      content: $('#quote-source').val(),
-      content: $('#quote-source-url').val()
-    },
+  // quote-submission-form
 
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader('X-WP-Nonce', api_vars.wpapi_nonce);
-    }
-  }).done(function(response) {
-    alert('Success! Comments are closed for this post.');
+  // Once the ID of the quote submission form, listen for the event for the quote-submission form.
+
+  $('#quote-submission-form').on('submit', function(event) {
+    event.preventDefault();
+    $.ajax({
+      method: 'post',
+      url: api_vars.rest_url + 'wp/v2/posts/',
+      data: {
+        status: 'pending',
+        title: $('#quote-author').val(),
+        content: $('#quote-content').val(),
+        _qod_quote_source: $('#quote-source').val(),
+        _qod_quote_source_url: $('#quote-source-url').val()
+      },
+
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('X-WP-Nonce', api_vars.wpapi_nonce);
+      }
+    })
+      .done(function(response) {
+        $('.quote-submission').slideUp();
+        $('.entry-title').html('Congratulations, you have submitted a quote!');
+      })
+      .fail(function() {
+        alert('Sorry, post could not be sent. Please try again');
+      });
   });
-
-  /**
-   * Ajax-based front-end post submissions.
-   */
-
-  //  The code bellow should be })(jQuery);
-  //
 })(jQuery);
